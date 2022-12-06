b6a97923a0d376573bc08476c947a97057ad1951
Yslan Ramos <yslanramos.r@gmail.com>
2022-12-06 20:04:31 -0300

    fix: corretly relative path

--- codemod/absoluteImportToRelativeImportTransform.ts
+++ codemod/absoluteImportToRelativeImportTransform.ts
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
-import { API, FileInfo, Options } from "jscodeshift";
-import { relative as pathRelative } from "path";
 
-const SOURCE_FOLDER = "src";
+import { API, FileInfo, Options } from "jscodeshift";
+import path from "path";
+
+const ROOT_FOLDER = "src";
+const ROOT_PATH = path.resolve(ROOT_FOLDER) + "/";
 
 const absoluteImportToRelativeImportTransform = (
   file: FileInfo,
@@ -10,21 +12,32 @@ const absoluteImportToRelativeImportTransform = (
   _options: Options
 ) => {
   const j = api.jscodeshift;
-  const filePath = file.path;
+  const filePath = path.resolve(ROOT_PATH, file.path.replace(ROOT_FOLDER, ""));
 
   return j(file.source)
     .find(j.ImportDeclaration)
     .filter((node) => {
       const absolutePath = node.value.source.value;
       return (
-        typeof absolutePath === "string" &&
-        absolutePath.startsWith(SOURCE_FOLDER)
+        typeof absolutePath === "string" && absolutePath.startsWith(ROOT_FOLDER)
       );
     })
-    .forEach((path) => {
-      j(path).replaceWith((node) => {
-        const absolutePath = node.value.source.value;
-        const relativePath = pathRelative(filePath, absolutePath);
+    .forEach((astPath) => {
+      j(astPath).replaceWith((node) => {
+        const absolutePath = path.join(
+          ROOT_PATH,
+          node.value.source.value.replace(ROOT_FOLDER, "") + ".js"
+        );
+
+        const isRootFolder =
+          path.dirname(filePath) === path.resolve(ROOT_FOLDER);
+
+        const relativePath = path
+          .relative(filePath, absolutePath)
+          .replace(/\.(js|ts)$/i, "")
+          // root  ../ -> ./
+          // subfolder ../../ -> ../
+          .slice(isRootFolder ? 1 : 3);
 
         return j.importDeclaration(
           node.value.specifiers,
--- output/index.js
+++ output/index.js
@@ -1,4 +1,4 @@
-import { bar } from "../bar/bar";
-import { foo } from "../foo";
+import { bar } from "./bar/bar";
+import { foo } from "./foo";
 
 console.log(bar, foo);
